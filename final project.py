241809526f88a76c072e3a01b1fe6b32fe98df2d
ckaczmarskyj <ckaczmarskyj@gmail.com>
2021-12-15 10:19:08 -0500

    defined lists & created main()

---
+++ final project.py
@@ -0,0 +1,17 @@
+"""This code is a wishing simulator that allows the user to get pokemon through wishing. Users can get more wishes by answering math questions correctly!"""
+
+print("Welcome to the pokemon wishing simulator!")
+#welcome statement
+
+pokemon_list_rare: list[str] = []
+#defined list of rare pokemon
+
+pokemon_list_ultra_rare: list[str] = []
+#defined list of ultra rare pokemon
+
+pokemon_list_legendary: list[str] = []
+#defined list of legendary pokemon
+
+def main():
+    #main function
+    return()
