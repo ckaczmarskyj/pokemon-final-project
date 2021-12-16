5020d9c29885f083bc3223ffde1235b82d454236
ckaczmarskyj <ckaczmarskyj@gmail.com>
2021-12-16 12:03:40 -0500

    added annotations

--- final project.py
+++ final project.py
@@ -20,45 +20,54 @@ pokemon_list_all: list[str] = ["Chansey", "Tapu Fini", "Dracovish","Quagsire","R
 
 def main():
     #main function
+
     wish = input("\nWould you like to wish? Input 'y' for yes, and 'n' for no: ")
+    #user input to ask if they would like to wish
 
     wish_count = 3
     user_pokemon_list: list[str] = []
-    #defined list of user's pulled pokemon
+    #defined current wish count and list of user's pulled pokemon
 
     while wish == 'y':
         
-        pokemon_choice = random.choices(pokemon_list_all, weights=(70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 20, 20, 20, 20, 20, 20, 20, 20, 20, 10, 10, 10), k=1)
+        pokemon_choice = random.choices(pokemon_list_all, weights=(40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 25, 25, 25, 25, 25, 25, 25, 25, 25, 20, 20, 20), k=1)
         pokemon_choice_str = pokemon_choice[0]
+        #chooses a random pokemon from the total list using weights for probability. 6% chance for legendary, 21% chance for ultra rare, 73% chance for legendary
         
         if pokemon_choice_str in pokemon_list_rare: 
             print("You got RARE pokemon", pokemon_choice_str)
             pokemon_choice_str = "[RARE] " + pokemon_choice_str
             user_pokemon_list += [pokemon_choice_str]
+            #return for rare pokemon
 
         elif pokemon_choice_str in pokemon_list_ultra_rare:
             print("You got ULTRA RARE pokemon", pokemon_choice_str)
             pokemon_choice_str = "[ULTRA RARE] " + pokemon_choice_str
             user_pokemon_list += [pokemon_choice_str]
+            #return for ultra rare pokemon
 
         elif pokemon_choice_str in pokemon_list_legendary: 
             print("WOW! You got LEGENDARY pokemon", pokemon_choice_str)
             pokemon_choice_str = "[LEGENDARY] " + pokemon_choice_str
             user_pokemon_list += [pokemon_choice_str]
+            #return for legendary pokemon
         
         
         wish_count -= 1
+        #takes one wish away when user wishes
         
         wish = input("\nWould you like to wish again? Input 'y' for yes, and 'n' for no: ")
 
         if wish != 'y':
             print("\nHave a nice day! Here is your pokemon list: ", user_pokemon_list)
             break
+            #if user does not want to continue, prints out list
 
         if wish_count == 0:
 
             math_question = input("\nuh oh! you have no wishes left :( would you like to answer a math question to solve it? Enter 'y' for yes and 'n' for no: ")
-            
+            #if user is out of wishes, they can answer a math question to get another
+
             if math_question == 'y':
                 a = random.randint(0, 20)
                 b = random.randint(0, 20)
@@ -69,6 +78,7 @@ def main():
                     wish_count += 1
                     print("\nCorrect! Good job :) You now have 1 wish")
                     wish = input("\nWould you like to wish again? Input 'y' for yes, and 'n' for no: ")
+                    #user is correct => option to continue game
                     
                     if wish != 'y':
                         print("\nHave a nice day! Here is your pokemon list: ", user_pokemon_list)
@@ -78,8 +88,11 @@ def main():
                     print("\nUh oh! Your answer is incorrect. Here is your pokemon list: ")
                     print(user_pokemon_list)
                     break
+                    #user is incorrect => game over
+
             else:
                 print("\nHave a nice day! Here is your pokemon list: ", user_pokemon_list)
                 break
+                #if user does not want to answer a math question, program ends, and they get their list of pokemon
 
 main()
